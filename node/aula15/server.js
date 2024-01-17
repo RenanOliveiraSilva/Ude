95e4e7748feb537eb1a06025b95af9896894bd30
RenanOliveiraSilva <renandeoliveirasilva123@gmail.com>
2024-01-17 09:24:16 -0300

    Session e Flash messages

--- node/aula15/server.js
+++ node/aula15/server.js
@@ -9,6 +9,10 @@ mongoose.connect(process.env.CONNECTIONSTRING).then(() => {
     app.emit('pronto');
 }).catch( e => console.log(e));
 
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const flash = require('connect-flash');
+
 const routes = require('./routes');
 const path = require('path');
 const { meuMiddleWare } = require('./src/middlewares/middleware');
@@ -16,6 +20,21 @@ const { meuMiddleWare } = require('./src/middlewares/middleware');
 app.use(express.urlencoded({ extended: true })); //Tratamento de POST
 
 app.use(express.static(path.resolve(__dirname, 'public'))); //Diretório Estático
+
+const sessionOptions = session({
+    secret: 'dwvvrewgerwgrwg qualquer',
+    store: MongoStore.create({ mongoUrl: process.env.CONNECTIONSTRING }),
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+        httpOnly: true
+    }
+});
+
+app.use(sessionOptions);
+app.use(flash());
+
 app.set('views', path.resolve(__dirname, 'src', 'views')); //Diretório de views
 app.set('view engine', 'ejs'); //Engine de view
 
--- node/aula15/src/controllers/homeController.js
+++ node/aula15/src/controllers/homeController.js
@@ -1,4 +1,18 @@
 exports.paginaIncial = (request, response) => {
+    //SESSION -> Irá durar o tempo programado nesse caso 7 dias, mesmo apagando o código ele ainda estará salvo
+    //request.session.usuario = { nome: "Ly", logado:true };
+    //console.log(request.session.usuario);
+
+    //FLASH -> Irá acontecer somente uma vez
+    // request.flash('error', 'msg de erro');
+    // request.flash('info', 'msg de alerta');
+    // request.flash('success', 'msg de sucesso');
+
+    //Saída do FLASH
+    // console.log(request.flash('error'))
+    // console.log(request.flash('info'))
+    // console.log(request.flash('success'))
+
     response.render('index'); //N precisa da extensão ejs
 }
 
